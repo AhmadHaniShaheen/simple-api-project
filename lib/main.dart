39df2d83231790b16fd863f360e5f3a00910336f
ahmadhanialishaheen@gmail.com <ahmadhanialishaheen@gmail.com>
2023-05-07 19:05:05 +0300

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,20 @@ import 'package:api_secand_project/screens/index_image.dart';
 import 'package:api_secand_project/screens/launch_screen.dart';
 import 'package:api_secand_project/screens/login_screen.dart';
 import 'package:api_secand_project/screens/password/forget_password_screen.dart';
-import 'package:api_secand_project/screens/password/rest_password_screen.dart';
 import 'package:api_secand_project/screens/register_screen.dart';
 import 'package:api_secand_project/screens/upload_image.dart';
 import 'package:api_secand_project/screens/users_screen.dart';
 import 'package:api_secand_project/storage/sharedPrefController.dart';
 import 'package:flutter/material.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPrefController().initPref();
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
@@ -24,16 +23,15 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/launch_screen',
       routes: {
-        '/launch_screen':(context)=>LaunchScreen(),
-        '/login_screen':(context)=>LoginScreen(),
-        '/forget_password_screen':(context)=>ForgetPasswordScreen(),
+        '/launch_screen': (context) => const LaunchScreen(),
+        '/login_screen': (context) => const LoginScreen(),
+        '/forget_password_screen': (context) => const ForgetPasswordScreen(),
         // '/rest_password_screen':(context)=>RestPasswordScreen(/*email: email*/),
-        '/register_screen':(context)=>RegisterScreen(),
-        '/users_screen':(context)=>UsersScreen(),
-        '/index_image':(context)=>IndexImages(),
-        '/upload_image':(context)=>UploadImage(),
+        '/register_screen': (context) => const RegisterScreen(),
+        '/users_screen': (context) => const UsersScreen(),
+        '/index_image': (context) => const IndexImages(),
+        '/upload_image': (context) => const UploadImage(),
       },
     );
   }
 }
-
